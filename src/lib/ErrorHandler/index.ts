76a9158772c6b3dedaa4cd819eee4e86098b72fa
Paulius <paulius.michelevicius@gmail.com>
2023-03-06 12:15:46 +0000

    L3-24: Basic logger lib.

---
+++ src/lib/ErrorHandler/index.ts
@@ -0,0 +1,42 @@
+import Ex from 'express'
+import { ValidateError } from 'tsoa'
+
+import log from 'logger'
+
+export interface Response {
+  message: string 
+  [name: string]: unknown
+}
+
+export class CustomError extends Error {
+  readonly message: string
+  readonly code: number
+
+  constructor(message: string, code: number) {
+    super(message)
+    this.message = message 
+    this.code = code || 500
+  }
+}
+
+export default function(
+  err: Error,
+  req: Ex.Request,
+  res: Ex.Response,
+  next: Ex.NextFunction,
+): Ex.Response | void {
+  log.debug('error occured', { err, req })
+
+  // handle TSOA validations
+  if (err instanceof ValidateError) return res.status(422).send({
+    ...err,
+    details: err.fields
+  })
+
+  if (err instanceof CustomError) return res.status(err.code).send({
+    ...err,
+    message: err.message
+  })
+
+  return next()
+}
